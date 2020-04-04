c084cd68fe59dd2d8734b8158924dfaa2d18b84e
RoniLeskinen <59826949+RoniLeskinen@users.noreply.github.com>
2020-04-04 10:32:22 +0300

    Version 0.1
    Start Page completed. Tested, works 100%.
    
    - Backround image added.
    - Navbar coded.

---
+++ GasStationApp/GasStationApp/StartPage.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace GasStationApp
+{
+    partial class StartPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.uSERMENUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lOGINToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sHUTDOWNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uSERMENUToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // uSERMENUToolStripMenuItem
+            // 
+            this.uSERMENUToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lOGINToolStripMenuItem,
+            this.sHUTDOWNToolStripMenuItem});
+            this.uSERMENUToolStripMenuItem.Name = "uSERMENUToolStripMenuItem";
+            this.uSERMENUToolStripMenuItem.Size = new System.Drawing.Size(116, 29);
+            this.uSERMENUToolStripMenuItem.Text = "USERMENU";
+            // 
+            // lOGINToolStripMenuItem
+            // 
+            this.lOGINToolStripMenuItem.Name = "lOGINToolStripMenuItem";
+            this.lOGINToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.lOGINToolStripMenuItem.Text = "LOGIN";
+            this.lOGINToolStripMenuItem.Click += new System.EventHandler(this.lOGINToolStripMenuItem_Click);
+            // 
+            // sHUTDOWNToolStripMenuItem
+            // 
+            this.sHUTDOWNToolStripMenuItem.Name = "sHUTDOWNToolStripMenuItem";
+            this.sHUTDOWNToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.sHUTDOWNToolStripMenuItem.Text = "SHUTDOWN";
+            this.sHUTDOWNToolStripMenuItem.Click += new System.EventHandler(this.sHUTDOWNToolStripMenuItem_Click);
+            // 
+            // StartPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GasStationApp.Properties.Resources.Start_Page;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "StartPage";
+            this.Text = "Start Page";
+            this.Load += new System.EventHandler(this.StartPage_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem uSERMENUToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lOGINToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sHUTDOWNToolStripMenuItem;
+    }
+}
+
---
+++ GasStationApp/GasStationApp/StartPage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GasStationApp
+{
+    public partial class StartPage : Form
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void StartPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Start of method
+        // Start page --> Usermenu --> Login button
+        // Clicking this button will take the user from "Start page" to "Login page". 
+
+        private void lOGINToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+
+            if (login.IsDisposed)
+            {
+                login = new Login();
+            }
+
+            login.Show();
+            this.Hide();
+        }
+
+        // End of last method
+
+        // Start of method
+        // Start page --> Usermenu --> Shutdown button
+        // Clicking this button will close the program.
+
+        private void sHUTDOWNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure?", "Program is closing", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Program was not closed", "Program is still running");
+            }
+        }
+
+        // End of last method
+    }
+}
